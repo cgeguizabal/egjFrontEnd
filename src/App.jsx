1f431def75fbe950314ecc8da2639bb4d63745c9
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-09-26 01:49:03 -0600

    added user data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import { useSession, useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { getUser } from "./assets/API/Services/UserInstance";
+import useAuthStore from "./store/auth";
 // import Navbar from "./components/NavBar";
 function App() {
+  const [token, setToken] = useState(null);
+  const [userData, setUserData] = useState(null);
+
+  // Zustand Auth Store
+  const logIn = useAuthStore((state) => state.login);
+  const logOut = useAuthStore((state) => state.logout);
+
+  // Clerk Hooks
+  const { user } = useUser();
+  const { session } = useSession();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (!user || !session) return;
+
+      try {
+        const token = await session.getToken();
+        setToken(token);
+
+        const data = await getUser(user.id, token);
+        setUserData(data);
+        // console.log("User Data:", data.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, [user, session]); // only runs when user or session changes
+
+  useEffect(() => {
+    if (user && userData && token) {
+      logIn(userData.data, token);
+    } else {
+      logOut();
+    }
+  }, [user, userData, token]); // runs when user, userData, or token changes
+
   return (
     <>
       {/* <Navbar /> */}
