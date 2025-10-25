8f310b1a4d54d62e053696d522631df15ac4d434
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-10-24 19:21:31 -0600

    FAQ

--- src/components/tourExpeditions/Faq.jsx
+++ src/components/tourExpeditions/Faq.jsx
@@ -3,7 +3,21 @@ import FaqStyle from "../../styles/components/tourExpeditions/fqa.module.scss";
 function Faq() {
   return (
     <div className={FaqStyle.container}>
-      <h1>Frequently Asked Questions</h1>
+      <div className={FaqStyle.text_container}>
+        <h1 className={FaqStyle.text_title}>
+          Expeditions George of the Jungle – FAQ
+        </h1>
+        <p className={FaqStyle.text_description}>
+          We’re here to help you. Contact us anytime. Ready to explore the
+          Amazon? Secure your spot with a 30% deposit today!
+        </p>
+        <hr className={FaqStyle.hr} />
+      </div>
+      <div className={FaqStyle.question_container}>
+        <div className={FaqStyle.question}>
+          <h3>Question</h3>
+        </div>
+      </div>
     </div>
   );
 }
